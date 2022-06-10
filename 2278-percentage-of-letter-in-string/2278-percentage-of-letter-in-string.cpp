e395f367f1d2bf16a153265b2181fc9e0c559c3f
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-06-10 15:09:42 +0800

    Time: 5 ms (18.04%), Space: 6.2 MB (43.82%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int percentageLetter(string s, char letter) {
+        string::iterator begin = s.begin(),end = s.end();
         int count = 0;
-        for(int i = 0; i < s.length() ; i++){
-            if(s[i] == letter) count++;
+        while(begin != end){
+            if(*begin == letter) count++;
+            begin++;
         }
         return 100*count/s.length();
             
