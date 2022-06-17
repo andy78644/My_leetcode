2a4f4f743c10f72c54349f317aa3fb2d606b88a5
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-06-17 17:55:05 +0800

    Time: 19 ms (17.09%), Space: 13 MB (79.29%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -23,21 +23,15 @@ public:
                 pair<int, int> point = curr.front();
                 curr.pop();
                 int i = point.first, j = point.second;
-                if(i-1>=0 && grid[i-1][j]==1) {grid[i-1][j]=2; curr.push({i-1, j});}
-                if(j-1>=0 && grid[i][j-1]==1) {grid[i][j-1]=2; curr.push({i, j-1});}
-                if(i+1<grid.size() && grid[i+1][j]==1) {grid[i+1][j]=2; curr.push({i+1, j});}
-                if(j+1<grid[0].size() && grid[i][j+1]==1) {grid[i][j+1]=2; curr.push({i, j+1});}
+                if(i-1>=0 && grid[i-1][j]==1) {grid[i-1][j]=2; curr.push({i-1, j}); fresh--;}
+                if(j-1>=0 && grid[i][j-1]==1) {grid[i][j-1]=2; curr.push({i, j-1}); fresh--;}
+                if(i+1<grid.size() && grid[i+1][j]==1) {grid[i+1][j]=2; curr.push({i+1, j}); fresh--;}
+                if(j+1<grid[0].size() && grid[i][j+1]==1) {grid[i][j+1]=2; curr.push({i, j+1}); fresh--;}
                 
             }
         }
+        if(fresh!=0) return -1;
         
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){            
-                    return -1;
-                }
-            }
-        }
         
         return count;
     }
