c43bb78cf4e4c639ded995d36d35ac7cc0dc04c3
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-06-20 15:59:06 +0800

    Time: 401 ms (8.42%), Space: 14.4 MB (93.56%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-    bool cmp(string a, string b){
-        return a.length() > b.length();
-    }
     int minimumLengthEncoding(vector<string>& words) {
         string ans;
         sort(words.begin(),words.end(), [](string &a,string &b){return a.size() > b.size();});
