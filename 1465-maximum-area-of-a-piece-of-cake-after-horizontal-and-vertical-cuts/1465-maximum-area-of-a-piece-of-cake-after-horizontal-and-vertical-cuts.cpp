a55dffd491173865d4839d6ce9e47d39a062f5a5
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-07-02 15:03:05 +0800

    Time: 94 ms (65.29%), Space: 32.2 MB (76.96%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts)     {
+        //vector<int> horidiff, vertidiff;
+        //horidiff.push_back(horizontalCuts[0]);
+        //vertidiff.push_back(verticalCuts[0]);
+        sort(horizontalCuts.begin(),horizontalCuts.end());
+        sort(verticalCuts.begin(),verticalCuts.end());
+        long int horimax = horizontalCuts[0];
+        long int vertimax = verticalCuts[0];
+        
+        for(int i=0;i<horizontalCuts.size()-1;i++){
+            int max = horizontalCuts[i+1] - horizontalCuts[i];
+            horimax = horimax>=max?horimax:max;
+        }
+        for(int i=0;i<verticalCuts.size()-1;i++){
+            int max = verticalCuts[i+1] - verticalCuts[i];
+            vertimax = vertimax>=max?vertimax:max;
+        }
+        int max = h-horizontalCuts[horizontalCuts.size()-1];
+        //cout << max << endl;
+        horimax = (horimax>=max)?horimax:max;
+        max = w-verticalCuts[verticalCuts.size()-1];
+        vertimax = (vertimax>=max)?vertimax:max;
+        //cout << vertimax << horimax;
+        return ((horimax%(1000000007))*(vertimax%(1000000007)))%(1000000007);
+        //horidiff.push_back(horizontalCuts[0]);
+        //vertidiff.push_back(verticalCuts[0]);
+    }
+};
