98017136e13a8324a54e6579e41dc7bcfb92bbb0
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-07-15 22:00:45 +0800

    Time: 18 ms (92.24%), Space: 24 MB (29.56%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -8,14 +8,10 @@ public:
             check[i].resize(n, 0);
         }
         int max = 0;
-        //cout << check[0].size();
-        //cout << m;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                //cout << grid[i][j];
                 if(check[i][j] || !grid[i][j]) continue;
                 int tmp = search(grid, i, j, check);
-                //cout << tmp << endl;
                 max = max>tmp?max:tmp;
             }
         }
@@ -24,13 +20,10 @@ public:
     int search(vector<vector<int>>& grid, int m,int n, vector<vector<int>>& check){
         int ans = 1;
         check[m][n] = 1;
-        //cout << m << n;
         if(m-1 >= 0 && grid[m-1][n] == 1 && check[m-1][n] == 0) ans += search(grid, m-1, n, check); 
-        //cout << ans;
         if(m+1 < grid.size() && grid[m+1][n] == 1 && check[m+1][n] == 0) ans += search(grid, m+1, n, check);
         if(n-1 >= 0 && grid[m][n-1] == 1 && check[m][n-1] == 0) ans += search(grid, m, n-1, check);
         if(n+1 < grid[0].size()  && grid[m][n+1] == 1 && check[m][n+1] == 0) ans += search(grid, m, n+1, check);
-        //cout << ans << endl;
         return ans;
     }
 };
