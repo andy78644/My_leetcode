b8a1d07ec43fa0fb32477937d4dfde2865da4db8
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-07-15 22:03:47 +0800

    Time: 21 ms (84.06%), Space: 23.2 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        //int m = grid.size();
+        //int n = grid[0].size();
         int max = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j] != 1) continue;
                 int tmp = search(grid, i, j);
                 max = max>tmp?max:tmp;
@@ -16,7 +16,6 @@ public:
     int search(vector<vector<int>>& grid, int m,int n){
         int ans = 1;
         grid[m][n] = -1;
-        
         if(m-1 >= 0 && grid[m-1][n] == 1) ans += search(grid, m-1, n); 
         if(m+1 < grid.size() && grid[m+1][n] == 1) ans += search(grid, m+1, n);
         if(n-1 >= 0 && grid[m][n-1] == 1) ans += search(grid, m, n-1);
