a2fb3f941b6a05e273d6e6438ace796afd8cf76a
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-07-26 15:53:21 +0800

    Time: 20 ms (73.61%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,29 +9,30 @@
  */
 class Solution {
 public:
-    TreeNode* ans = NULL;
+    //TreeNode* ans = NULL;
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         //TreeNode* ans = root;
         //TreeNode* check = q
-        find(root, p, q);
-        return ans;
+        if(root == NULL || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left && right) return root;
+        else if(!left) return right;
+        else return left;
     }
-    bool find(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(root == NULL) return false;
-        int left = find(root->left, p, q)?1:0;
-        int right = find(root->right, p, q)?1:0;
-        int mid = 0;
-        if(root == p || root == q) mid =1;
-        if(left + right + mid == 2){
-            ans = root;
-        }
-        return (left + right + mid > 0);
-        
-    }
+    
 };
 
 /*
 分別去找兩棵子樹
 如果各找到一個則為答案
 或是該節點本身為答案 配上某子樹有答案
+*/
+/*
+dfs for
+using stack
+
+
+
+
 */
