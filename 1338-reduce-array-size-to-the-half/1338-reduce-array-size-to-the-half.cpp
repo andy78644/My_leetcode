765a8eb1bb3726a3716df0f7a2a26b2e60b26e9a
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-08-19 00:47:25 +0800

    Time: 496 ms (16.42%), Space: 80 MB (45.72%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        map<int, int> check;
+        int size = arr.size()/2;
+        for(auto i: arr){
+            if(check[i]==0) check[i] = 1;
+            else check[i]++;
+        }
+        //cout << check[3];
+        vector<int> tmp;
+        for(const auto &i: check){
+            tmp.push_back(i.second);
+        }
+        sort(tmp.begin(), tmp.end(), greater());
+        int ans = 0;
+        int k = 0;
+        while(ans < size){
+            ans += tmp[k];
+            k++;
+        }
+        return k;
+    }
+};
