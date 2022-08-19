cbce3986a4865650e03c64efc5f5d9dca2076172
andy78644 <46518318+andy78644@users.noreply.github.com>
2022-08-19 20:59:50 +0800

    Time: 187 ms (40.02%), Space: 59.1 MB (20.90%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -7,14 +7,15 @@ public:
         }
         for(auto k: nums){
             if(count[k] == 0) continue;
+            count[k]--;
             if(end[k-1]>0){
                 end[k-1]--;
                 end[k]++;
-                count[k]--;
+                //count[k]--;
             }
             else if(count[k+1] && count[k+2]){
                 end[k+2]++;
-                count[k]--;
+                //count[k]--;
                 count[k+1]--;
                 count[k+2]--;
             }
